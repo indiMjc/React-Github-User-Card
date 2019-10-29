c359b5318ebee380293099912c51bc3c062a4218
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-10-29 17:23:25 -0400

    followers populated via second api, no styling

--- github-usercards/src/components/Usercard.js
+++ github-usercards/src/components/Usercard.js
@@ -3,34 +3,40 @@ import axios from "axios";
 
 class Usercard extends Component {
   state = {
+    pic: {},
     name: "",
     login: "",
-    profileLink: ""
+    profileLink: "",
+    followers: []
   };
-  componentDidMount() {
-    axios
-      .get("https://api.github.com/users/indiMjc")
-      .then(res => {
-        console.log(res);
-        console.log(res.data.avatar_url);
-        console.log(res.data.name);
-        console.log(res.data.login);
-        console.log(res.data.html_url);
-        this.setState({
-          name: res.data.name,
-          login: res.data.login,
-          profileLink: res.data.html_url
-        });
-      })
-      .catch(err => {
-        console.log("The data was not returned", err);
-      });
+  async componentDidMount() {
+    const [userRes, followerRes] = await Promise.all([
+      axios.get("https://api.github.com/users/indiMjc"),
+      axios.get("https://api.github.com/users/indiMjc/followers")
+    ]);
+    this.setState({
+      pic: userRes.data.avatar_url,
+      name: userRes.data.name,
+      login: userRes.data.login,
+      profileLink: userRes.data.html_url,
+      followers: followerRes.data
+    });
   }
 
   render() {
     return (
-      <div>
-        <h1>test div</h1>
+      <div className="card-contain">
+        <img src={this.state.pic} alt="User" />
+        <h1>Name: {this.state.name}</h1>
+        <h2>Login: {this.state.login}</h2>
+        <a href={this.state.profileLink}>Github</a>
+        <h2>Followers:</h2>
+        {this.state.followers.map(follower => (
+          <div className="followers" key={follower.id}>
+            <a href={follower.html_url}>{follower.login}</a>
+            <br />
+          </div>
+        ))}
       </div>
     );
   }
