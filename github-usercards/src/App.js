ec5f20549c80f703aef497390f7476f4739a4e4c
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-10-29 15:58:42 -0400

    created empty usercard componenet with test div

--- github-usercards/src/App.js
+++ github-usercards/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Usercard from "./components/Usercard";
 
 function App() {
-  return <div className="App">eyy</div>;
+  return (
+    <div className="App">
+      <Usercard />
+    </div>
+  );
 }
 
 export default App;
---
+++ github-usercards/src/components/Usercard.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Usercard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>test div</h1>
+      </div>
+    );
+  }
+}
+
+export default Usercard;
