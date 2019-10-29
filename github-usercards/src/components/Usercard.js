af3b287cf2404b54e0ffbf04ae94fb44fc4432bd
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-10-29 16:02:44 -0400

    setup class structure for Usercard component and also componenetDidMount lifecycle method for API call, console.log API data

--- github-usercards/src/components/Usercard.js
+++ github-usercards/src/components/Usercard.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Usercard extends Component {
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/indiMjc")
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log("The data was not returned", err);
+      });
+  }
+
   render() {
     return (
       <div>
